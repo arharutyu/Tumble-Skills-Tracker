1c51b33bd11528c9c7dc524b597095218df2c1fd
arharuty <13937@coderacademy.edu.au>
2023-08-18 19:51:46 +1000

    add contentcards with links

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import ContentCard from '../components/ContentCard';
 
 const Home = () => {
-  return (
-    <div>Home</div>
+  return ( <>
+    <h1>Home</h1>
+    <Row xs={1} md={2} lg={4} className="g-4">
+        <Col>
+          <ContentCard title="Students" text="students" image="" link="students" />
+        </Col>
+        <Col>
+          <ContentCard title="Skills" text="skills" image="" link="skills" />
+        </Col>
+        <Col>
+          <ContentCard title="New Assessment" text="new assessment" image="" link="new" />
+        </Col>
+        <Col>
+          <ContentCard title="Users" text="users" image="" link="users" />
+        </Col>
+    </Row>
+  </>
   )
 }
 
