4c743505856dcdc551b1d7971ca3684402e9863d
arharuty <13937@coderacademy.edu.au>
2023-08-18 16:57:34 +1000

    change to bootstrap react component

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () => {
   return (
-
-    <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" href="#">
-          <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="100"></img>
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
-            <Link className="nav-link" href="#">Students</Link>
-            <Link className="nav-link" href="#">Skills</Link>
-            <Link className="nav-link" href="#">New Assessment</Link>
-            <Link className="nav-link disabled" aria-disabled="true">Users</Link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
+    <Navbar expand="md" fixed="top" >
+        <Navbar.Brand href="#home"><img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="100"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav variant="pills" defaultActiveKey="/home" className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Students</Nav.Link>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">New Assessment</Nav.Link>
+            <Nav.Link href="#link" eventKey="disabled" disabled>Users</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavBar
