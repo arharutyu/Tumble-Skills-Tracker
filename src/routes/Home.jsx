fcb9e4824cac17a5d51e43497a150a8d2c97729c
arharuty <13937@coderacademy.edu.au>
2023-08-20 21:57:36 +1000

    cards rendered based on isadmin Bool

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,24 +5,54 @@ import Row from 'react-bootstrap/Row';
 import ContentCard from '../components/ContentCard';
 
 const Home = () => {
+  let isAdmin = false
+
+  let cards = [{
+    title: "Students", 
+    text:"See all current students, search for a student, and view inidividual student profiles.",
+    image:"",
+    link:"students"
+  },
+  {
+    title: "Skills", 
+    text:"See all current skills and levels.",
+    image:"",
+    link:"skills"
+  },
+  {
+    title: "New Assessment", 
+    text:"Create new assessment.",
+    image:"",
+    link:"new"
+  }
+  ]
+
+  const usersCard =   {
+    title: "Users", 
+    text:"See all user details.",
+    image:"",
+    link:"users"
+  }
+
+  let lgBp = 3
+
+  if (isAdmin) {
+    cards.push(usersCard)
+    lgBp ++
+  }
+
   return ( <>
     <h1>Home</h1>
-    <Row xs={1} md={2} lg={4} className="g-4">
+    <Row xs={1} md={2} lg={lgBp} className="g-4">
+      {cards.map(card => (
         <Col>
-          <ContentCard title="Students" text="See all current students, search for a student, and view inidividual student profiles." image="" link="students" />
+          <ContentCard title={card.title} text={card.text} image={card.image} link={card.link} />
         </Col>
-        <Col>
-          <ContentCard title="Skills" text="skills" image="" link="See all current skills and levels." />
-        </Col>
-        <Col>
-          <ContentCard title="New Assessment" text="Create new assessment." image="" link="new" />
-        </Col>
-        <Col>
-          <ContentCard title="Users" text="See all user details." image="" link="users" />
-        </Col>
+      ))}
     </Row>
   </>
   )
 }
+
 
 export default Home
