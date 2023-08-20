72714837447ff56be0b3e3a677087a332778039b
arharuty <13937@coderacademy.edu.au>
2023-08-20 23:51:09 +1000

    add body bg, component border, responsive sizing

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import './Login.css'
+import { Row } from 'react-bootstrap'
 
 const Login = () => {
   const nav = useNavigate()
@@ -18,23 +21,31 @@ const Login = () => {
       nav('/')
     }}
 
+
+    // TODO: Fix logo sizing when resizing window (esp large size)
+
   return (<>
-    <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="300"></img>
-    <h1>Tumble Skills Tracker Login</h1>
-    <br></br>
-    <Form onSubmit={submit}>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Username:</Form.Label>
-      <Form.Control type="input" placeholder="Enter username" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+  <Container fluid="sm" id='login'>
+    <Row>
+      <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" max-width="300"></img>
+      <h1>Tumble Skills Tracker Login</h1>
+    </Row>
+    <Row>
+      <Form onSubmit={submit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="input" placeholder="Enter username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </Row>
+  </Container>
   </>
   )
 }
