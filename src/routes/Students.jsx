a98e5b69ce7656001fd8473b0c9096d22b97910f
arharuty <13937@coderacademy.edu.au>
2023-08-21 03:25:53 +1000

    build cards grid based on admin & student array

--- src/routes/Students.jsx
+++ src/routes/Students.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import StudentCard from '../components/StudentCard';
 
 const Students = () => {
-  return (
-    <div>Students</div>
+  let isAdmin = true
+
+  let students = [
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' },
+    { name: 'John' }
+  ]
+
+  const addStudent = { name: 'Add Student' }
+
+  if (isAdmin) {
+    students.unshift(addStudent)
+  }
+
+  return ( <>
+    <h1>Students</h1>
+    <Row xs={1} md={4} lg={6} className="g-4">
+      {students.map(student => (
+        <Col>
+          <StudentCard name={student.name} />
+        </Col>
+      ))}
+    </Row>
+    </>
   )
 }
 
