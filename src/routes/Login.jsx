0953b052e555218a0381e4f7c11a99a7ffde4b0e
arharuty <13937@coderacademy.edu.au>
2023-08-21 10:37:28 +1000

    add fetch call for log in

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,50 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import './Login.css'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 const Login = () => {
   const nav = useNavigate()
 
-  let testUser = [{
-    username: "test",
-    password: "password"
-  }]
-
-  function submit(event) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  async function submit(event) {
     // TODO: Make this work
     event.preventDefault()
-    if ((event.target.username === testUser.username) && (event.target.password === testUser.password)) {
-      nav('/')
-    }}
+    const authEndpoint = 'http://localhost:4001/auth/login';
 
+    // Create a request payload
+    const credentials = {
+      username: username,
+      password: password,
+    };
 
-    // TODO: Fix logo sizing when resizing window (esp large size)
+    // Send a POST request to the authentication endpoint
+    await fetch(authEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+    .then(response => {
+      if (response.ok) {
+        // Get the JWT token from the response header
+        const token = response.headers.get('accessToken');
+        // Store the token in local storage or state for later use
+        // For example, you can use localStorage.setItem('token', token);
+        nav('/');
+      } else {
+        // Handle authentication error
+         
+        console.error('Authentication failed');
+      }
+    })
+    .catch(error => {
+      console.error('Error during authentication:', error);
+    });
+  }
+  
 
   return (<>
-  <Container fluid="md" id='login'>
-        <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="250"></img>
-      <h1>Tumble Skills Tracker Login</h1>
-      <Form onSubmit={submit} id='login-form'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="input" placeholder="Enter username" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  </Container>
-  </>
-  )
-}
+    <Container fluid="md" id='login'>
+          <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="250"></img>
+        <h1>Tumble Skills Tracker Login</h1>
+        <Form onSubmit={submit} id='login-form'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="input" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+    </>
+    )
+  }
 
 export default Login
