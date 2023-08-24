2e04364d17c15e5523e10f59e05de160a131aa97
arharuty <13937@coderacademy.edu.au>
2023-08-24 12:33:10 +1000

    fix links to avoid refresh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,16 +8,16 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <Navbar className="navbar-custom" expand="md" fixed="top" >
-        <Navbar.Brand href="#home" className="custom-logo"><img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="100"></img></Navbar.Brand>
+        <Navbar.Brand className="custom-logo"><img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="100"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Container>
           <Nav fill variant="pills" className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/students">Students</Nav.Link>
-            <Nav.Link href="/skills">Skills</Nav.Link>
-            <Nav.Link href="/new">New Assessment</Nav.Link>
-            <Nav.Link href="/users" eventKey="disabled" disabled>Users</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/students">Students</Nav.Link>
+            <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
+            <Nav.Link as={Link} to="/new">New Assessment</Nav.Link>
+            <Nav.Link as={Link} to="/users" eventKey="disabled" disabled>Users</Nav.Link>
           </Nav>
           </Container>
         </Navbar.Collapse>
