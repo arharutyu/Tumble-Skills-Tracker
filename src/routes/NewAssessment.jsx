22736fb8c330d21bf1ca2433efef823acc213066
arharuty <13937@coderacademy.edu.au>
2023-08-24 17:26:07 +1000

    add search students

--- src/routes/NewAssessment.jsx
+++ src/routes/NewAssessment.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import '../components/App.css'
+import SearchText from '../components/SearchText'
+import { STUDENTS } from '../api/endpoints'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const NewAssessment = () => {
+  const [searchStudents, setSearchStudents] = useState([])
+
   return (
     <>
   <Container className="contcontainer">
     <h1>New Assessment</h1>
+    <SearchText text="Search for a student" endpoint={STUDENTS} set={setSearchStudents}  />
+    <ListGroup>
+      {searchStudents.map((student, index) => (
+        <ListGroup.Item key={index}>{student.name}</ListGroup.Item>
+      ))
+      }
+    </ListGroup>
   </Container>
   </>
   )
