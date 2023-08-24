273ea1ccd47328c10872fad6066645d50d9a3b98
arharuty <13937@coderacademy.edu.au>
2023-08-25 05:27:01 +1000

    navigate to student page on successful submit

--- src/components/SubmitAsssessment.jsx
+++ src/components/SubmitAsssessment.jsx
@@ -2,23 +2,38 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { post } from '../api/api'
 import { ASSESSMENTS } from '../api/endpoints'
+import { useNavigate } from 'react-router-dom'
 
 const SubmitAsssessment = ({ assessed, student }) => {
+  // Initialize the navigation function from React Router
+  const nav = useNavigate()
+  
+  // Function to handle the submit assignment button click
   async function submit(event) {
     event.preventDefault()
-    console.log(assessed)
-    console.log(student)
+    // console.log(assessed)
+    // console.log(student)
+
+    // Create the assessment object with student ID and assessed skills
     const assessment = {
       "student": student,
       "isCompleted": true,
       "skills": [...assessed]
     }
-    console.log(assessment)
+    // console.log(assessment)
+
     try {
-      await post(ASSESSMENTS, assessment); // Await the post function call
-      console.log("Assessment submitted successfully");
-      
+      // Make a POST request to create the assessment on the server
+      const response = await post(ASSESSMENTS, assessment)
+      // Parse the JSON response returned by the API
+      const createdAssessment = await response.json()
+
+      // Log success message and navigate to the student's page
+      console.log("Assessment submitted successfully", createdAssessment)
+      nav(`/students/${createdAssessment.student}`)
+
     } catch (error) {
+      // Handle errors and log error message
       console.error("Error submitting assessment:", error);
     }
   }
