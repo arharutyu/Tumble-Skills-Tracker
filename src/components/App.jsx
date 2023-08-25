0a58d1733dfd3b8d8da6a3037c63df2d8345beb0
arharuty <13937@coderacademy.edu.au>
2023-08-25 22:27:07 +1000

    add student form component, route in app created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ import Login from '../routes/Login'
 import StudentProfile from '../routes/StudentProfile'
 import StartAssessment from '../routes/StartAssessment'
 import EditStudent from '../routes/EditStudent'
+import AddStudent from '../routes/AddStudent'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -43,13 +44,19 @@ function App() {
           
           <Routes>
             <Route path="/" element={<Home isAdmin={user.isAdmin} name={user.name} />} />
+            
             <Route path="/students" element={<Students isAdmin={user.isAdmin} />} />
+            <Route path="/students/new" element={<AddStudent isAdmin={user.isAdmin} />} />
             <Route path="/students/:id" element={<StudentProfile isAdmin={user.isAdmin} />} />
             <Route path="/students/:id/edit" element={<EditStudent isAdmin={user.isAdmin} />} />
+            
             <Route path="/skills" element={<Skills />} />
+            
             <Route path="/new" element={<NewAssessment />} />
             <Route path="/new/start" element={<StartAssessment />} />
+            
             <Route path="/users" element={<Users />} />
+            
             <Route path="*" element={<h3>Page not found</h3>} />
           </Routes>
           
