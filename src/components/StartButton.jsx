21dcfd8227123cae074b616248107c18189c9d33
arharuty <13937@coderacademy.edu.au>
2023-08-29 20:50:28 +1000

    add loading state to start button

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import { useNavigate } from 'react-router-dom'
 
 const StartButton = ({student, assessSkills}) => {
   const nav = useNavigate()
 
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (isLoading) {
+      // Store the selected student and assessment skills in local storage
+      localStorage.setItem('student', JSON.stringify(student))
+      localStorage.setItem('assessment', JSON.stringify(assessSkills))
+      setIsLoading(false)
+      // Navigate to the assessment start page
+      nav('/new/start')
+    }
+  }, [isLoading])
+
   // Function to handle the click of the assessment start button
   async function submit(event) {
     event.preventDefault()
-
-    // Store the selected student and assessment skills in local storage
-    localStorage.setItem('student', JSON.stringify(student))
-    localStorage.setItem('assessment', JSON.stringify(assessSkills))
-
-    // Navigate to the assessment start page
-    nav('/new/start')
+    setIsLoading(true)
   }
 
   return (
     <>
-      <Button variant="primary" onClick={submit}>Start Assessment</Button>
+      <Button variant="primary" onClick={submit}>
+      {isLoading? '...' : 'Start Assessment'}</Button>
     </>
   )
 }
