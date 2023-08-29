295cf864129513e12ab07b70b7551e9a23cfa0cc
mpichlmann <14205@coderacademy.edu.au>
2023-08-29 15:11:55 +1000

    added sass

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -56,6 +56,7 @@ const Login = ({ setIsLoggedIn, setUser }) => {
   
 
   return (<>
+  <Container fluid id="login-bg">
     <Container fluid="md" id='login'>
           <img src="https://eliteallstars.com.au/wp-content/uploads/2019/06/eliteAsset-2.png" alt="Logo" width="250"></img>
         <h1>Tumble Skills Tracker Login</h1>
@@ -73,6 +74,7 @@ const Login = ({ setIsLoggedIn, setUser }) => {
           Submit
         </Button>
       </Form>
+    </Container>
     </Container>
     </>
     )
