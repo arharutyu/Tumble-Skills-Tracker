64b3981d6079e3a393c5f2a8b6e4f165bd994997
LachlanPeterson <14383@coderacademy.edu.au>
2023-08-29 17:16:10 +1000

    Users and UserCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,39 +3,12 @@ import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Container from 'react-bootstrap/Container'
 
-const UserCard = ({ username, name, id, is_admin, role }) => {
-  let admin = "No"
-
-  if (is_admin) {
-    admin = "Yes"
-  }
-
-  return (
-    <>
+const UserCard = ({ name }) => {
+  return ( <>
     <Card>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{role}</Card.Subtitle>
       </Card.Body>
-      <Container>
-        <Card.Body>
-          <ListGroup variant="flush">
-            <ListGroup horizontal>
-              <ListGroup.Item>username:</ListGroup.Item>
-              <ListGroup.Item>{username}</ListGroup.Item>
-            </ListGroup>
-            <ListGroup horizontal>
-              <ListGroup.Item>Id:</ListGroup.Item>
-              <ListGroup.Item>{id}</ListGroup.Item>
-            </ListGroup>
-            <ListGroup horizontal>
-              <ListGroup.Item>Admin:</ListGroup.Item>
-              <ListGroup.Item>{admin}</ListGroup.Item>
-            </ListGroup>
-          </ListGroup>
-        </Card.Body>
-      </Container>
     </Card>
     </>
   )
--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import UserCard from '../components/UserCard'
 import SearchText from '../components/SearchText'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import StudentCard from '../components/StudentCard'
-import '../components/App.css'
+import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
+import '../components/App.css'
 import { get } from '../api/api.js'
 import { USERS } from '../api/endpoints'
+import AddCard from '../components/AddCard'
 
 const Users = ({isAdmin, accessToken}) => {
   const [users, setUser] = useState([])
@@ -21,25 +22,27 @@ const Users = ({isAdmin, accessToken}) => {
       setUser(data)
     })()
   }, [])
-
-
-
-
-
-
-  
   
   return (<>
   <Container className="contcontainer">
     <h1>Users</h1>
-    <SearchText text="Search for a user" />
-    <Row xs={1} md={3} lg={4} className="g-4">
-      <StudentCard name="Add new user" />
-      {users.map(user => (
-        <Col>
-          <UserCard name={user.name} username={user.username} id={user.id} is_admin={user.is_admin} role={user.role} />
+    <SearchText text="Search for a user" endpoint={USERS} set={setUser}  />
+    <Row xs={1} md={4} lg={6} className="g-4">
+      {isAdmin && (<>
+      <Col key="add"><AddCard type="User" link="/users/new" /></Col>
+      </>)}
+      {users.length > 0 && (<>
+      {users.map((user, index) => (
+        <Col key={index}>
+          <UserCard name={
+            <Link to={`/users/${user._id}`}>
+            {user.name} </Link>
+            } />
         </Col>
       ))}
+      </>
+      )
+      }
     </Row>
     </Container>
     </>
