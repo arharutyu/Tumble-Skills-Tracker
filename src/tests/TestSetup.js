27c203a413585c1a54d866b681073560b910f23b
arharuty <13937@coderacademy.edu.au>
2023-08-30 15:56:46 +1000

    no router render custom fx

--- src/tests/TestSetup.js
+++ src/tests/TestSetup.js
@@ -4,5 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 const customRender = (ui, options) =>
   render(ui, { wrapper: BrowserRouter, ...options })
 
+const noRouterRender = (ui, options) =>
+  render(ui, { ...options })
+
 export * from '@testing-library/react'
 export { customRender as render }
+export { noRouterRender }
