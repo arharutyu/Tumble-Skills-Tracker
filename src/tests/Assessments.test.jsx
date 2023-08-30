3e7e9cfcf6d8df6546f418908fca91cc74a1b6fd
arharuty <13937@coderacademy.edu.au>
2023-08-30 17:01:04 +1000

    check admin fx renders

--- src/tests/Assessments.test.jsx
+++ src/tests/Assessments.test.jsx
@@ -39,58 +39,71 @@ describe('Start Assessment component', () => {
 })
 
 describe('Start Assessment Button', () => {
- it('renders with assessment columns and submit button', () => {
+ it('renders with text', () => {
     render(<StartButton />)
     expect(screen.getByRole('button', {name: "Start Assessment"})).toBeInTheDocument()
   }) 
 })
 
 describe('View Assessment accordion', () => {
- it('renders with assessments', () => {
-    const assessments = [{
-      "_id": "64ec7d15e801599240bcbf11",
-      "student": {
-          "_id": "64ec7d15e801599240bcbef2",
-          "name": "Lachie"
-      },
-      "doneBy": {
-          "_id": "64ec7d15e801599240bcbef0",
-          "name": "Jay Son"
-      },
-      "skills": [
-          {
-              "skill": {
-                  "_id": "64ec7d14e801599240bcbed5",
-                  "skillName": "Handstand",
-                  "levels": [
-                      0
-                  ]
-              },
-              "score": 5,
-              "_id": "64ec7d15e801599240bcbf12"
-          },
-          {
-              "skill": {
-                  "_id": "64ec7d14e801599240bcbed6",
-                  "skillName": "Cartwheel",
-                  "levels": [
-                      0,
-                      1
-                  ]
-              },
-              "score": 5,
-              "_id": "64ec7d15e801599240bcbf13"
-          }
-      ],
-      "isCompleted": true,
-      "Date": "2022-01-01T00:00:00.000Z",
-      "__v": 0
-  }] 
-    render(<ViewAssessments assessments={assessments} />)
+  const assessments = [{
+        "_id": "64ec7d15e801599240bcbf11",
+        "student": {
+            "_id": "64ec7d15e801599240bcbef2",
+            "name": "Lachie"
+        },
+        "doneBy": {
+            "_id": "64ec7d15e801599240bcbef0",
+            "name": "Jay Son"
+        },
+        "skills": [
+            {
+                "skill": {
+                    "_id": "64ec7d14e801599240bcbed5",
+                    "skillName": "Handstand",
+                    "levels": [
+                        0
+                    ]
+                },
+                "score": 5,
+                "_id": "64ec7d15e801599240bcbf12"
+            },
+            {
+                "skill": {
+                    "_id": "64ec7d14e801599240bcbed6",
+                    "skillName": "Cartwheel",
+                    "levels": [
+                        0,
+                        1
+                    ]
+                },
+                "score": 5,
+                "_id": "64ec7d15e801599240bcbf13"
+            }
+        ],
+        "isCompleted": true,
+        "Date": "2022-01-01T00:00:00.000Z",
+        "__v": 0
+    }] 
+ 
+  it('renders with assessments', () => {
+    render(<ViewAssessments isAdmin={true} assessments={assessments} />)
     expect(screen.getByRole('heading', {name: "Saturday, 1 January 2022 at 11:00 am"})).toBeInTheDocument()
     expect(screen.getByRole('button', {name: "Saturday, 1 January 2022 at 11:00 am"})).toBeInTheDocument()
     expect(screen.queryAllByRole('row')).toHaveLength(3)
     expect(screen.queryAllByRole('columnheader')).toHaveLength(3)
+  }) 
+ 
+  it('renders with admin buttons if admin', () => {
+    render(<ViewAssessments isAdmin={true} assessments={assessments} />)
+    expect(screen.getByRole('button', {name: "Edit Assessment"})).toBeInTheDocument()
+    expect(screen.getByRole('button', {name: "Delete Assessment"})).toBeInTheDocument()
+  }) 
+
+  it('doesnt render admin buttons if not admin', () => {
+    render(<ViewAssessments isAdmin={false} assessments={assessments} />)
+    expect(screen.queryByRole('button', {name: "Edit Assessment"})).toBeNull()
+    expect(screen.queryByRole('button', {name: "Delete Assessment"})).toBeNull()
   }) 
 })
 
